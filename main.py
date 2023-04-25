a31769be724796d36e2339ef25f30d40843c4755
Jéssica Mora Cruz_Prueba 1 <emejmc09@gmail.com>
2023-04-24 20:58:33 -0600

    Se hace un llamado de funciones y se ejecuta el main

--- main.py
+++ main.py
@@ -1,64 +1,79 @@
-
 import os
-from Singleton import llamar_Singleton
-llamar_Singleton()
 
-## Se hace un llamado de la funcion y se incluye un archivo de texto de la reñesa de la banda
-from ArchivoTexto import realizar_Archivo_Texto
-from RegistroEstudiantes import registrarEstudiante, listadoEstudiantes,buscarEstudiante, modificarDatos, consultarHistorial
-realizar_Archivo_Texto("nuevo_archivo.txt", "La banda tiene historia desde el anio 2008, donde se han obtenido premios!")
-
+from RegistroEstudiantes import RegistroEstudiantes
+## Para limpiar pantalla
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
+## Metodo main
 def main():
+    ## Se da la bienvenida al usuario
     print("____________________________________________________________________________")
     print("                       Bienvenido estimado usuario")
     print("____________________________________________________________________________")
     print("")
-
+    ## Se imprimen los nombres de los profesores autorizados
     print("A continuacion se muestran los nombres de las personas autorizadas a llenar lo que se le solicita")
     print("Jose", "Julio", "Axel", "Jessica", "Ernesto")
     # Aquí se está utilizando una tupla de nombres que no va a variar mientras se ejecuta
     profesores_encargados = ["Jose", "Julio", "Axel", "Jessica", "Ernesto"]
 
-    nombres = {"Juan": 25, "María": 30, "Pedro": 40}
-    print(nombres["María"])  # Output: 30
+    ## Ejemplo de diccionario
+    nombres = {"Jose": 25, "Julio": 30, "Axel": 40, "Jessica" : 50, "Ernesto" : 60}
+    ## Se imprime el código del profesor Julio que representa su edad
+    print(nombres["Julio"])  # Output: 30
+    ## Se solicita que ingrese el nombre del profesor que está editando
     nombre = input("Estimado profesor por favor ingrese su nombre: ")
     if nombre in profesores_encargados:
         print("Usted está autorizado a editar, ya que se encuentra en la siguiente lista:")
-
+    ## Ejemplo de singleton como si existieran dos registros
+    registro_de_estudiantes_1 = RegistroEstudiantes()
+    registro_de_estudiantes_2 = RegistroEstudiantes()
+    ## Se realiza un while para repetir el menú cada vez que el usuario indica que si desea volver a ver el menpú
     while True:
         clear()
+        ## Se imprimen las opciones del menú
         print("Por favor elija una de las siguientes opciones:");
+        ## los primeros números representan el registro de estudiantes
         print("1.- Ingresar datos de estudiantes")
-        print("2.- Mostrar datos de integrante")
-        print("3.- Buscar Integrante")
+        print("2.- Mostrar datos de estudiantes")
+        print("3.- Buscar estudiante")
         print("4.- Modificar Instrumento")
         print("5.- Consultar Instrumento Modificado")
+        ## luego se realiza por medio de una interfaz el inventario de instrumentos
         print("6.- Realizar inventario de instrumentos")
-        print("7.- Calcular promedios de notas")
-        print("8.- Imprimir historial de banda")
-        print("9.- Eliminar el historial de la banda")
+        ## luego se realiza por medio de una interfaz el cálculo de promedios de estudiantes
+        print("7.- Calcular promedios de notas de estudiante")
+        ## Se imprime el historial de los datos de estudiantes ingresados en el archivo de texto
+        print("8.- Imprimir historial de estudiantes")
+        ## Se elimina el historia en el archivo de texto
+        print("9.- Eliminar el historial de estudiantes")
         print("10.- Salir\n")
 
         opcion = int(input("Opcion: "))
-
+        ## según la opción se hace un llamado de la funcion
         if opcion == 1:
-            registrarEstudiante()
+            registro_de_estudiantes_1.registrarEstudiante()
         elif opcion == 2:
-            listadoEstudiantes()
+            registro_de_estudiantes_2.listadoEstudiantes()
         elif opcion == 3:
-            buscarEstudiante()
+            registro_de_estudiantes_1.buscarEstudiante()
         elif opcion == 4:
-            modificarDatos()
+            registro_de_estudiantes_1.modificarDatos()
         elif opcion == 5:
-            consultarHistorial()
+            registro_de_estudiantes_1.consultarHistorial()
         elif opcion == 6:
-            from InterfazInventario import llamar_interfaz
-            llamar_interfaz()
+            try:
+                from InterfazInventario import llamar_interfaz
+                llamar_interfaz()
+            except:
+                print("")
         elif opcion == 7:
-            from InterfazPromedio import Calcular_promedio
-            Calcular_promedio()
+            ## se utilizan excepciones
+            try:
+                from InterfazPromedio import Calcular_promedio
+                Calcular_promedio()
+            except:
+                print("")
         elif opcion == 8:
             # Abre el archivo de texto en modo lectura
             archivo = open("nuevo_archivo.txt", "r")
@@ -73,19 +88,22 @@ def main():
             archivo.close()
         elif opcion == 9:
             # Nombre del archivo que deseas eliminar
-            nuevo_archivo = "archivo.txt"
+            nuevo_archivo = "nuevo_archivo.txt"
 
-            # Eliminar el archivo
-            os.remove(nuevo_archivo)
+            # Elimina la información del archivo
+            with open(nuevo_archivo, 'w') as archivo:
+                archivo.write('')
+
         elif opcion == 10:
             break
         # Pregunta si se desea volver al menu
-        i = input("\nDesea volver al menu principal (s/n). Si elige NO se le mostrara informacion de instrumentos BMA")
+        i = input("\nDesea volver al menu principal (s/n). Si elige NO se le mostrara informacion de instrumentos BMA\n")
         if i == 'n' or i == 'N':
             break
 
 if __name__ == "__main__":
     main()
+    ## Se hace un llamado de la información utilizada con el patrón abstract factory
     from PatronAbstractFactory import InformacionVentaInstrumentos, programa_aleatorio
     programa = InformacionVentaInstrumentos(programa_aleatorio)
     for i in range(5):
